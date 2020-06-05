0f05ad501542642a76c3a258af3de3db6cc4785a
Javi Rando <javier.rando01@estudiant.upf.edu>
2020-06-05 17:50:59 +0200

    Splitting losses

--- model.py
+++ model.py
@@ -1,62 +1,9 @@
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
 from constants import device, content_layers_default, style_layers_default, previous_layers_default
 import copy
 import torch.optim as optim
-
-
-# ================================== LOSS ================================== #
-
-def gram_matrix(input):
-    a, b, c, d = input.size()  # a=batch size(=1)
-    # b=number of feature maps
-    # (c,d)=dimensions of a f. map (N=c*d)
-
-    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
-
-    G = torch.mm(features, features.t())  # compute the gram product
-
-    # we 'normalize' the values of the gram matrix
-    # by dividing by the number of element in each feature maps.
-    return G.div(a * b * c * d)
-
-
-class ContentLoss(nn.Module):
-    def __init__(self, target, ):
-        super(ContentLoss, self).__init__()
-        # we 'detach' the target content from the tree used
-        # to dynamically compute the gradient: this is a stated value,
-        # not a variable. Otherwise the forward method of the criterion
-        # will throw an error.
-        self.target = target.detach()
-
-    def forward(self, input):
-        self.loss = F.mse_loss(input, self.target)
-        return input
-
-
-class StyleLoss(nn.Module):
-
-    def __init__(self, target_feature):
-        super(StyleLoss, self).__init__()
-        self.target = gram_matrix(target_feature).detach()
-
-    def forward(self, input):
-        G = gram_matrix(input)
-        self.loss = F.mse_loss(G, self.target)
-        return input
-
-
-class PreviousLoss(nn.Module):
-    def __init__(self, previous_frame):
-        super(PreviousLoss, self).__init__()
-        self.target = previous_frame.detach()  # We use the previous stylized frame as target
-
-    def forward(self, input):
-        self.loss = F.mse_loss(input, self.target)
-        return input
-
+from loss import StyleLoss, ContentLoss, PreviousLoss
 
 # ================================== NORMALIZATION ================================== #
 
